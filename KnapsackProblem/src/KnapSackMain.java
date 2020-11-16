7bfd182d69820b2ee2627b41451cd63c3e0dccca
BeauCranston <beau2tie@gmail.com>
2020-11-16 01:25:21 -0500

    retdfg

--- KnapsackProblem/src/KnapSackMain.java
+++ KnapsackProblem/src/KnapSackMain.java
@@ -3,12 +3,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
-//This assignment was done by Beau Cranston 000397019
-
-///Stat to optimize:  WISDOM
 
 public class KnapSackMain {
     
@@ -76,15 +72,15 @@ public class KnapSackMain {
     static final String[] attribs = {"HP","MN","MV","DR","HR","STR","DEX","CON","INT","WIS","LCK"};
     static String desiredAttribute = attribs[9];
     //static int W = 6;  // class example
-    //weight = gold that we have
     static int W = 1000; // assignment code
     //static boolean DEBUG = true;  //46 seconds - shows helper output
-    static boolean DEBUG = true; // 3 seconds
+    static boolean DEBUG = false; // 3 seconds
 
     public static void main(String[] args) {
         // uncomment the lines noted as class example to see the solution to the knapsack that was done
         // manually in the lecture.  Be sure to comment out the lines labelled assignment code
-        String path= "dataFile.csv";
+
+        String path="dataFile.csv";
         try {
             Scanner sc = new Scanner(new File(path));
         } catch(Exception e) {
@@ -98,8 +94,8 @@ public class KnapSackMain {
             System.out.println("No items found, aborting.");
             System.exit(255);
         }
-        //force memo item table to only have 2 rows
-        MemoItem[][] m = new MemoItem[2][W+1];
+        
+        MemoItem[][] m = new MemoItem[it.length][W+1];
         
         for ( int j = 0; j <= W; j++ ) {
             m[0][j] = new MemoItem();
@@ -111,37 +107,34 @@ public class KnapSackMain {
                     // can't take it
                     debugWrite( String.format("Can't take item %d, bag too "+
                             "small\n", i) );
-                    m[1][j] = m[0][j];
+                    m[i][j] = m[i-1][j];
                 } else {
-                    if ( m[0][j].value > m[0][j-it[i].weight].value +
+                    if ( m[i-1][j].value > m[i-1][j-it[i].weight].value + 
                             it[i].value ) {
                         // don't take it
                         debugWrite( String.format("Won't take item %d, " + 
                                 "previous bag is worth more\n", i));
-                        m[1][j] = m[0][j];
+                        m[i][j] = m[i-1][j];
                     } else {
                         debugWrite( String.format("Taking item %d, addition" +
                                 " increases bag value\n", i));
                         ArrayList<Items> l = (ArrayList<Items>)
-                                (m[0][j-it[i].weight].list.clone());
+                                (m[i-1][j-it[i].weight].list.clone());
                         l.add(it[i]);
                         //l.add(it[i].name);
-                        m[1][j] = new MemoItem( m[0][j-it[i].weight].value + it[i].value, l);
-                        m[0][j] = m[1][j];
+                        m[i][j] = new MemoItem( m[i-1][j-it[i].weight].value + it[i].value, l);
                     }
                 }
             }
             String result = "";
-            for ( MemoItem in : m[1] )
-                result += in.value + "\t";
+            for ( MemoItem in : m[i] )
+                result += Double.toString(in.value) + "\t";
             debugWrite( result );
         }
         String result = "";
-        for ( Items s : m[1][W].list )
+        for ( Items s : m[it.length-1][W].list )
             result += "\t" + s.name + "\n";
-        System.out.printf("\nThe optimal value is %d\nThe contents of the "+
-                "bag are:\n%s",m[1][W].value,result);
-        System.out.println("MemoItems \n Rows: " +  m.length + " Cols: " + m[0].length);
+        System.out.printf("The optimal value is %d\nThe contents of the "+
+                "bag are:\n%s",m[it.length-1][W].value,result);
     }
-
 }
