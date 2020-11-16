cde009a7d9d4059e9bc12e1568d362f1f958ed13
BeauCranston <beau2tie@gmail.com>
2020-11-16 03:53:43 -0500

    sECOND PART complete

--- KnapsackProblem/src/KnapSackMain.java
+++ KnapsackProblem/src/KnapSackMain.java
@@ -10,12 +10,19 @@ import java.util.*;
 ///Stat to optimize:  WISDOM
 
 public class KnapSackMain {
-
+    //This assignment was done by Beau Cranston 000397019
     public static void debugWrite( String s ) {
         if ( DEBUG )
             System.out.print( s );
     }
 
+    /**
+     * gets the item data from the csv, calculates its Attribute based on Attributes included and stat weight.
+     * The method then converts the data to an Item object which is stored in a Item Array that it returns
+     * @param pathToCSV file to get data from
+     * @param desiredAttributes the desired Attributes to optimize. The key is the name of the Attribute, and the value is it's weight
+     * @return
+     */
     public static Items[] getItems(String pathToCSV, HashMap<String, Integer> desiredAttributes) {
         // adapted from https://stackabuse.com/reading-and-writing-csvs-in-java/
         BufferedReader csvReader;
@@ -23,24 +30,17 @@ public class KnapSackMain {
         String row;
 
         try {
+            //read csv
             ArrayList<Items> al = new ArrayList<>();
             csvReader = new BufferedReader( new FileReader(pathToCSV));
             // consume header row
             csvReader.readLine();
+            //read file
             while ((row = csvReader.readLine() ) != null ) {
                 String[] data = row.split(",");
-                int value=-1;
-
-                for( int a = 0; a < data.length; a++ ) {
-                    String stat = data[a];
-                    if (desiredAttributes.containsKey(stat)) {
-                        // assume each attribute only exists one time per item
-                        int statWeight = desiredAttributes.get(stat).intValue();
-                        System.out.println(statWeight);
-                        value += statWeight * Integer.parseInt(data[a+1]);
-                        break;
-                    }
-                }
+                //get the value of the item based on the desired attributes and their weights
+                int value = getItemValue(data, desiredAttributes);
+                //if the value is valid, add the item to the list
                 if ( value != -1 )
                     al.add ( new Items( Integer.parseInt(data[2]), value , data[1] ) );
             }
@@ -57,6 +57,31 @@ public class KnapSackMain {
         }
 
         return items;
+    }
+
+    /**
+     * Gets the items value based on the desired attributes passed to it. Append the value of each attribute to the running total for the item.
+     * @param data
+     * @param desiredAttributes a hashmap containing the name of the attribute as a key, and the weight of the attribute as the value.
+     * @return
+     */
+    public static int getItemValue(String[]data, HashMap<String,Integer>desiredAttributes){
+        //initialize to -1 to check for an invalid item
+        int value=-1;
+        for( int a = 0; a < data.length; a++ ) {
+            String stat = data[a];
+            //if the stat is included in the desired attributes hashmap
+            if (desiredAttributes.containsKey(stat)) {
+                // assume each attribute only exists one time per item
+                //get the weight of the attribute
+                int statWeight = desiredAttributes.get(stat).intValue();
+                //add the value for that stat to the overall value of the item
+                value += statWeight * Integer.parseInt(data[a+1]);
+                break;
+            }
+        }
+
+        return value;
     }
 
     public static Items[] getItems() {
@@ -81,22 +106,23 @@ public class KnapSackMain {
     //weight = gold that we have
     static int W = 1000; // assignment code
     //static boolean DEBUG = true;  //46 seconds - shows helper output
-    static boolean DEBUG = true; // 3 seconds
+    static boolean DEBUG = false; // 3 seconds
 
     public static void main(String[] args) {
         // uncomment the lines noted as class example to see the solution to the knapsack that was done
         // manually in the lecture.  Be sure to comment out the lines labelled assignment code
         String path= "dataFile.csv";
-        HashMap<String, Integer> statsToOptimize = new HashMap<>();
-        statsToOptimize.put(attribs[9], 5);
-        statsToOptimize.put(attribs[9], 5);
+
         try {
             Scanner sc = new Scanner(new File(path));
         } catch(Exception e) {
             System.out.println("file exception");
         }
+        HashMap<String, Integer> statsToOptimize = new HashMap<>();
+        //THIS IS WHERE YOU CHOOSE THE ATTRIBUTES AND THEIR WEIGHTS
+        statsToOptimize.put(attribs[9].toLowerCase(), 5);
+        statsToOptimize.put(attribs[10].toLowerCase(), 5);
 
-        //Items[] it = getItems(); // class example
         Items[] it = getItems( path, statsToOptimize ); // assignment code
 
         if ( it == null ) {
@@ -106,16 +132,19 @@ public class KnapSackMain {
         //force memo item table to only have 2 rows
         MemoItem[][] m = new MemoItem[2][W+1];
 
+        //initialize first row
         for ( int j = 0; j <= W; j++ ) {
             m[0][j] = new MemoItem();
         }
 
+        //changed code to only include the 0 and 1 index. 0 Index is the index being compared, and index 1 is the result of an optimization taking place
         for ( int i=1; i<it.length; i++ ) {
             for (int j = 0; j <= W; j++ ) {
                 if ( it[i].weight > j ) {
                     // can't take it
                     debugWrite( String.format("Can't take item %d, bag too "+
                             "small\n", i) );
+                    //if the item is too heavy copy it down from the row before it
                     m[1][j] = m[0][j];
                 } else {
                     if ( m[0][j].value > m[0][j-it[i].weight].value +
@@ -123,6 +152,7 @@ public class KnapSackMain {
                         // don't take it
                         debugWrite( String.format("Won't take item %d, " +
                                 "previous bag is worth more\n", i));
+                        //if the value does not improve, copy the value from the previous row
                         m[1][j] = m[0][j];
                     } else {
                         debugWrite( String.format("Taking item %d, addition" +
@@ -136,18 +166,30 @@ public class KnapSackMain {
                     }
                 }
             }
+            //the items at index 1 now become the row to check against
             m[0] = Arrays.copyOf(m[1], m[1].length);
-            String result = "";
-            for ( MemoItem in : m[1] )
-                result += in.value + "\t";
-            debugWrite( result );
+//            String result = "";
+//            for ( MemoItem in : m[1] )
+//                result += in.value + "\t";
+//            debugWrite( result );
+
         }
+        outputResults(m, statsToOptimize);
+    }
+    public static void outputResults(MemoItem[][] m, HashMap<String, Integer> attributes){
         String result = "";
         for ( Items s : m[1][W].list )
             result += "\t" + s.name + "\n";
+        StringBuilder sb = new StringBuilder();
+        for(String key : attributes.keySet()){
+            sb.append(key+ ", ");
+        }
+
+        System.out.println("Attributes Chosen: " + sb.toString());
         System.out.printf("\nThe optimal value is %d\nThe contents of the "+
                 "bag are:\n%s",m[1][W].value,result);
         System.out.println("MemoItems \n Rows: " +  m.length + " Cols: " + m[0].length);
     }
+
 
 }
